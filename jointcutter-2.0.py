b499e6c523c026b3ddf120836ac948783786328f
PiSaucer <alansaucer@outlook.com>
2019-04-14 17:12:57 -1000

    update

--- jointcutter-2.0.py
+++ jointcutter-2.0.py
@@ -15,6 +15,7 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 import time
 
+
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 print(filename)
@@ -209,4 +210,5 @@ command = "ffmpeg -framerate "+str(frameRate)+" -i "+TEMP_FOLDER+"/newFrame%06d.
 subprocess.call(command, shell=True)
 
 deletePath(TEMP_FOLDER)
+os.rmdir("TEMP")
 
